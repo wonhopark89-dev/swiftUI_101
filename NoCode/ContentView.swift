ea92748a9bab13587a222d51641069adb3df99ec
wonhopark89-dev <wonhopark89.dev@gmail.com>
2023-10-07 14:19:46 +0900

    Initial Commit

---
+++ NoCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoCode
+//
+//  Created by wonhopark on 10/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoCode/NoCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoCodeApp.swift
+//  NoCode
+//
+//  Created by wonhopark on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct NoCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
